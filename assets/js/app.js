4b16b7324905e3fc8402712cc780efccc43d34bd
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-03-27 10:44:02 +0530

    nav color update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,15 @@
-const menuicon = document.querySelector("menuicon");
-const menulist = document.querySelector("menulist");
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menulist");
 
 
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("showlist");
 })
+
+
+// const menuicon = document.querySelector(".menuicon");
+// const menulist = document.querySelector(".menulist");
+
+// menuicon.addEventListener("click", function () {
+//     menulist.classList.toggle("showlist")
+// })
